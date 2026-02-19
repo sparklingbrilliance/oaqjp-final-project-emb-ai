a0d573881c5d4b8b83ef8ddeb2563ab234b0bdd1
sparklingbrilliance <mailadityanow@gmail.com>
2026-02-19 16:15:56 +0530

    Add files via upload

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,18 @@
+import requests, json
+
+def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    result = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': [k for k, v in emotions.items() if v == max(emotions.values())]
+    }
+    return result
